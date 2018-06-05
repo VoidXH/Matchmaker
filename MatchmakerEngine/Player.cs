b251957a37597516054074ec3a70c064d5455cbe
VoidX <voidxb@gmail.com>
2018-06-05 17:46:16 +0200

    Weight contribution with win chance

--- MatchmakerEngine/Player.cs
+++ MatchmakerEngine/Player.cs
@@ -31,21 +31,23 @@
         /// <param name="Result">Match outcome</param>
         /// <param name="MatchType">Ranking weight modifier</param>
         public void Evaluate(float TeamSkill, float OpponentSkill, float Contribution, Results Result, MatchTypes MatchType) {
-            // TODO: Win chance must be a factor for non-team games, but it is not required for teams, as it doesn't change their contribution to their own team.
-            //float WinChance = MMath.Clamp01(OpponentSkill / TeamSkill);
-
-            // Contribution is weighted with skill difference, this has many advantages:
-            // - If the player is much more skilled than the team, he'll go up the ranks faster.
-            // - If the player deserved this high rank, but has a bad day, deranking will be slower.
-            // - If the player constantly fails to deliver an expected contribution for his level, he'll go down fast.
-            if (TeamSkill != 0)
-                Contribution = Mathf.Clamp01(MatchmakingRating / TeamSkill * Contribution);
+            if (TeamSkill != 0) {
+                Contribution = Mathf.Clamp01(Contribution *
+                    // Contribution is weighted with skill difference, this has many advantages:
+                    // - If the player is much more skilled than the team, he'll go up the ranks faster.
+                    // - If the player deserved this high rank, but has a bad day, deranking will be slower.
+                    // - If the player constantly fails to deliver an expected contribution for his level, he'll go down fast.
+                    MatchmakingRating / TeamSkill *
+                    // Contribution is also weighted with win chance.
+                    // A miracle victory gives lots of SR, while a loss against way better players is not punished.
+                    OpponentSkill / TeamSkill);
+            }
             float TeamContribBonus = Contribution * MedianGain * 2;
             if (Result == Results.Win)
                 SkillRating = Mathf.Clamp01(SkillRating + TeamContribBonus);
             else if (Result == Results.Loss)
                 SkillRating = Mathf.Clamp01(SkillRating + TeamContribBonus - MedianGain * 2);
-            MatchmakingRating = Mathf.Clamp01(SkillRating + (Contribution - .5f) * MedianGain * 4);
+            MatchmakingRating = Mathf.Clamp01(SkillRating + (Contribution - .5f) * MedianGain * (MatchType != MatchTypes.Placement ? 4 : 8));
 
             if (MatchType != MatchTypes.HighStakes)
                 SkillRating = MatchmakingRating; // When accurate rating over a larger timespan is not key, simply use MMR only.
